9e1204b9fccc39433dc02e9e8701239d7edaf9aa
Jordan Ye <79342877+Jordan231111@users.noreply.github.com>
2025-05-30 20:45:37 -0400

    Add initial HelloWorld.java file for collaboration demo

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    private String message;
+    
+    public HelloWorld() {
+        this.message = "Hello, World!";
+    }
+    
+    public void setMessage(String newMessage) {
+        this.message = newMessage;
+    }
+    
+    public String getMessage() {
+        return this.message;
+    }
+    
+    public void printMessage() {
+        System.out.println(this.message);
+    }
+    
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.printMessage();
+        
+        hello.setMessage("Welcome to Git collaboration!");
+        hello.printMessage();
+    }
+}
